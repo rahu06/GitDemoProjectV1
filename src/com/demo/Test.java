ef16190409e4a15b37db9467b457c7a993c2bd68
rahul <rahul.vidishaa@gmail.com>
2023-07-18 01:12:27 +0530

    First commit

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -1,9 +1,20 @@
 package com.demo;
 
 public class Test {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+String hungry="yes";
+if (hungry=="yes")
+{
+	System.out.println("eat something");
+	
+}
+else
+{
+	System.out.println("do work");
+}
 
 	}
 
