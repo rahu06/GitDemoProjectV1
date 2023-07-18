9e12c60778233cdb729fa5313e693fc2827e653b
rahul <rahul.vidishaa@gmail.com>
2023-07-18 11:14:05 +0530

    second commit

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 String hungry="yes";
-if (hungry=="yes")
+if (hungry=="yes"|| hungry=="Y")
 {
 	System.out.println("eat something");
 	
